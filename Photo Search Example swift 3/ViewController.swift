d75638a1aab76a57b1763111cb0f97cb140aefef
Victor Lo <Victor@victors-macbook-air.local>
2016-09-14 19:12:51 -0700

    Initial Commit

---
+++ Photo Search Example swift 3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Photo Search Example swift 3
+//
+//  Created by Victor Lo on 9/14/16.
+//  Copyright © 2016 Victor Lo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
